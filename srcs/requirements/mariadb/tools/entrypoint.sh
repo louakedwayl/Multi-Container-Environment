8dae70171d2ca21915d41017e84f1077405a7498
wayl <louakedwayl@protonmail.com>
2025-06-08 14:19:53 +0200

    Modification to enforce password when connecting to the database.

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
-set -e #si une commande echoue exit le script
+set -e
 
-# Démarrer MariaDB en arrière-plan
 mysqld_safe &
 
-# Récupérer le PID du dernier processus lancé en arrière-plan
 pid="$!"
 
-# Boucle until jusqu'a ce que la condition mysqladmin ping --silent soit vrai 
-# cad boucle sur le retour de ping Si MariaDB n’est pas prêt, la commande échoue 
-#(retourne un code différent de 0), donc la boucle continue.
-#Si MariaDB est prêt, la commande réussit (retourne 0), alors la boucle s’arrête.
-until mysqladmin ping --silent; do
+# Timeout de 30 secondes max
+timeout=30
+while ! mysqladmin ping --silent; do
     echo "Waiting for MariaDB to be ready..."
     sleep 1
+    timeout=$((timeout - 1))
+    if [ $timeout -le 0 ]; then
+        echo "Timeout waiting for MariaDB to start"
+        exit 1
+    fi
 done
 
-# Exécuter le script SQL d'initialisation
-mysql < /init.sql
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" < /init.sql
 
-# Attendre la fin du serveur MariaDB (en avant-plan)
 wait $pid
 
